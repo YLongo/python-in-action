22880c3c71d316a546d2b9f06f1c7009e302e461
YLongo <yhlona523@gmail.com>
2021-01-15 20:27:12 +0800

    chapter17
    Signed-off-by: YLongo <yhlona523@gmail.com>

--- chapter17/python_repos.py
+++ chapter17/python_repos.py
@@ -17,5 +17,14 @@ print("reposities returned:", len(repo_dicts))
 repo_dict = repo_dicts[0]
 print("\nkeys:", len(repo_dict))
 
-for key in sorted(repo_dict.keys()):
-    print(key)
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+
+print("\nSelected information about first repository:")
+print("Name:", repo_dict['name'])
+print("Owner:", repo_dict['owner']['login'])
+print("Stars:", repo_dict['stargazers_count'])
+print("Repository:", repo_dict['html_url'])
+print("Created:", repo_dict['created_at'])
+print("Updated:", repo_dict['updated_at'])
+print("Description:", repo_dict['description'])
