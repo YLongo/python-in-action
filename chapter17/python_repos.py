c4c30fb269780d7fdbb024ce76ca2b02787cdefa
YLongo <yhlona523@gmail.com>
2021-01-16 15:52:07 +0800

    chapter17
    Signed-off-by: YLongo <yhlona523@gmail.com>

--- chapter17/python_repos.py
+++ chapter17/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 
@@ -14,17 +16,22 @@ print("total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("reposities returned:", len(repo_dicts))
 
-repo_dict = repo_dicts[0]
-print("\nkeys:", len(repo_dict))
+names, stars = [], []
+
+# repo_dict = repo_dicts[0]
+# print("\nkeys:", len(repo_dict))
 
 # for key in sorted(repo_dict.keys()):
 #     print(key)
 
-print("\nSelected information about first repository:")
-print("Name:", repo_dict['name'])
-print("Owner:", repo_dict['owner']['login'])
-print("Stars:", repo_dict['stargazers_count'])
-print("Repository:", repo_dict['html_url'])
-print("Created:", repo_dict['created_at'])
-print("Updated:", repo_dict['updated_at'])
-print("Description:", repo_dict['description'])
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects in Github'
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
+
